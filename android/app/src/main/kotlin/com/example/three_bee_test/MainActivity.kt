1de6f6b7589071ed1b7adbf1fed7b55593e480ed
Riboldi, Andrea <ariboldi@emeal.nttdata.com>
2023-03-16 20:42:54 +0100

    init commit

---
+++ android/app/src/main/kotlin/com/example/three_bee_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.three_bee_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/login/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '3BeeTest',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
